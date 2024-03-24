af0621ab70aaeaf10f6123549be9f5ba7390493d
anirudhlakhotia <sanjeev196945@gmail.com>
2024-03-24 19:06:18 +0530

    init: Add files

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG21CS086 has successfully built and run the program\n";
+  return 0;
+}
